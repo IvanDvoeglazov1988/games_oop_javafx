2951f0d17a9e7e9d6ec5eb8031de0eb164a79b32
IvanDvoeglazov1988 <id@stk-online.ru>
2023-01-15 19:27:11 +0500

    2. Каркас шахматной доски [#792]

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -18,17 +18,40 @@ public class BishopBlack implements Figure {
 
     @Override
     public Cell[] way(Cell dest) {
-        throw new ImpossibleMoveException(
-                String.format("Could not way by diagonal from %s to %s", position, dest)
-        );
+        if (!isDiagonal(position, dest)) {
+            throw new ImpossibleMoveException(
+                    String.format("Could not way by diagonal from %s to %s", position, dest)
+            );
+        }
+        int size = dest.getX() - position.getX();
+        Cell[] steps = new Cell[size];
+        int deltaX = 1;
+        int deltaY = -1;
+        int x = position().getX() + deltaX;
+        int y = position().getY() + deltaY;
+        for (int index = 0; index < size; index++) {
+            steps[index] = Cell.findBy(x, y);
+            x = x + deltaX;
+            y = y + deltaY;
+        }
+        return steps;
     }
 
     public boolean isDiagonal(Cell source, Cell dest) {
-        return false;
+        int first = source.getX() - dest.getX();
+        int second = source.getY() - dest.getY();
+        if (first < 0) {
+            first = first * -1;
+        }
+        if (second < 0) {
+            second = second * -1;
+        }
+        return (first) == (second);
     }
 
     @Override
     public Figure copy(Cell dest) {
         return new BishopBlack(dest);
     }
+
 }
---
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.chess.firuges.black;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.chess.OccupiedCellException;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BishopBlackTest {
+
+    @Test
+    void whenPositionToException() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        assertThat(Cell.C1).isEqualTo(bishopBlack.position());
+    }
+
+    @Test
+    void whenСopyToCopyDest() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        Figure figure = bishopBlack.copy(Cell.A3);
+        assertThat(figure.position()).isEqualTo(Cell.A3);
+    }
+
+    @Test
+    void whenWayTo() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        Cell[] figure = bishopBlack.way(Cell.G5);
+        Cell[] exception = {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        assertThat(exception).isEqualTo(figure);
+    }
+
+}
